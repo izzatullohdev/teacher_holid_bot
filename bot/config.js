5b4af7d7661e9ccc22a4cc69cd130d0afb558b3d
izzatullohdev <izzatbekmadaminov7141@gmail.com>
2025-03-27 05:15:31 +0500

    yangi faillar qo'shildi va adminni tanlab oldik

--- bot/config.js
+++ bot/config.js
@@ -3,4 +3,5 @@ require("dotenv").config();
 module.exports = {
   token: process.env.BOT_TOKEN,
   channelId: process.env.CHANNEL_ID,
+  adminId: process.env.ADMIN_ID,
 };
--- bot/index.js
+++ bot/index.js
@@ -1,11 +1,38 @@
 const TelegramBot = require("node-telegram-bot-api");
-const { token, channelId } = require("./config");
-const photoHandler = require("./handlers/photoHandler");
-const textHandler = require("./handlers/textHandler");
+const { token, channelId, adminId } = require("./config");
 const bot = new TelegramBot(token, { polling: true });
 
-// Handlers
-photoHandler(bot, channelId);
-textHandler(bot, channelId);
+// Admin check
+bot.on("message", (msg) => {
+  const userId = msg.from.id;
+
+  if (userId !== Number(adminId)) {
+    bot.sendMessage(
+      msg.chat.id,
+      "⛔ Sizga ruxsat yo'q. darsliklar tez kunda chiqadi Youtbe kanalimizga obuna bo'ling \n\n youtube.com/@holid27 "
+    );
+    return;
+  }
+
+  // Admin bo‘lsa - handlerlar ishlaydi
+  if (msg.photo) {
+    require("./handlers/photoHandler")(bot, channelId, msg);
+  } else if (msg.text && !msg.text.startsWith("/")) {
+    require("./handlers/textHandler")(bot, channelId, msg);
+  }
+});
+
+// /start komandasi
+bot.onText(/\/start/, (msg) => {
+  const isAdmin = msg.from.id === Number(adminId);
+  if (isAdmin) {
+    bot.sendMessage(
+      msg.chat.id,
+      "👋 Salom admin! Siz kanalga post yuborishingiz mumkin."
+    );
+  } else {
+    bot.sendMessage(msg.chat.id, "⛔ Bu bot faqat admin uchun.");
+  }
+});
 
 console.log("🤖 Bot ishga tushdi...");
